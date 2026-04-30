282af92bbb0cd4c1bca3c24ff06468b124eb6302
allantorres83stos-ops <allantorres83stos@gmail.com>
2026-04-30 00:15:42 -0400

    add chart package

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Spinner } from '@/components/ui/spinner';
+import { PieChart } from "react-native-gifted-charts";
 
 export default function HomeScreen() {
   return (
@@ -15,6 +16,14 @@ export default function HomeScreen() {
         <ThemedText style={styles.caption}>
           Main screen spinner rendered with gluestack.
         </ThemedText>
+      </ThemedView>
+
+      <ThemedView style={styles.spinnerCard}>
+        <PieChart
+          data={[{ value: 62, color: "#CBB4F3"}, { value: 38, color: "#EDE7F6" }]}
+          donut
+          radius={60}
+        />
       </ThemedView>
     </ThemedView>
   );
