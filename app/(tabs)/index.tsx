be50c28d817fbdcee395528133f71ce2bcc6d061
allantorres83stos-ops <allantorres83stos@gmail.com>
2026-04-30 11:27:54 -0400

    add file picker package

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
-import { StyleSheet } from 'react-native';
+import * as DocumentPicker from "expo-document-picker";
+import React, { useState } from "react";
+import { Button, StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
@@ -6,9 +8,30 @@ import { Spinner } from '@/components/ui/spinner';
 import { PieChart } from "react-native-gifted-charts";
 
 export default function HomeScreen() {
+  const [file, setFile] = useState(null);
+
+  const pickDocument = async () => {
+    try {
+      let result = await DocumentPicker.getDocumentAsync({
+        type: "*/*", // you can restrict: "application/pdf", "image/*", etc.
+        copyToCacheDirectory: true,
+        multiple: false,
+      });
+      console.log(result);
+    } catch (error) {
+      console.log("Error picking document:", error);
+    }
+  };
+
   return (
     <ThemedView style={styles.container}>
       <ThemedView style={styles.spinnerCard}>
+        <Button title="Pick a document" onPress={pickDocument} />
+        {file && (
+          <>
+            <ThemedText style={styles.caption}>Picked!!</ThemedText>
+          </>
+        )}
         <Spinner size="large" color="#38bdf8" />
         <ThemedText type="title" style={styles.title}>
           Loading
